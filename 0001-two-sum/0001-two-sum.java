ef77f2cfd3d9070002f2e0b2225e872462cd1d3e
Joo Won Kim <joocode12@gmail.com>
2024-03-28 03:11:17 +0900

    Time: 8 ms (54.49%), Space: 44.7 MB (73.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,12 +1,29 @@
 class Solution {
 	public int[] twoSum(int[] nums, int target) {
+
+		int[][] sortedNums = new int[nums.length][2];
+
 		for (int i = 0; i < nums.length; i++) {
-			for (int j = i + 1; j < nums.length; j++) {
-				if (target == nums[i] + nums[j]) {
-					return new int[] {i , j};
-				}
+			sortedNums[i][0] = nums[i];
+			sortedNums[i][1] = i;
+		}
+
+		Arrays.sort(sortedNums, (a, b) -> a[0] - b[0]);
+
+		int left = 0;
+		int right = nums.length - 1;
+
+		while (left < right) {
+			int sum = sortedNums[left][0] + sortedNums[right][0];
+			if (target > sum) {
+				left++;
+			} else if (target < sum) {
+				right--;
+			} else {
+				return new int[] { sortedNums[left][1], sortedNums[right][1] };
 			}
 		}
+
 		return new int[] {};
 	}
 }
