dcb9d14921cd43c228ba3ad62b432b358a846d05
Joo Won Kim <joocode12@gmail.com>
2024-03-28 01:58:01 +0900

    Time: 50 ms (11.22%), Space: 44.9 MB (43.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,21 +1,12 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> numMap = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-
-            if (numMap.containsKey(complement)) {
-
-                return new int[]{numMap.get(complement), i};
-            }
-
-            numMap.put(nums[i], i);
-        }
-
-        return new int[]{};
-    }
+	public int[] twoSum(int[] nums, int target) {
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (target == nums[i] + nums[j]) {
+					return new int[] {i , j};
+				}
+			}
+		}
+		return new int[] {};
+	}
 }
